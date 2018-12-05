ec95e4f4c47c3d2c5ddac91710d655b3de2ce640
daytonyoung <36241804+daytonyoung@users.noreply.github.com>
2018-12-05 16:12:41 -0500

    Changed "Selling out for"

--- components/TargetReps.vue
+++ components/TargetReps.vue
@@ -37,7 +37,7 @@
            class="rep-container">
         <div class="sml-push-y2 lrg-push-y0 sml-pad-1 fill-warn is-rounded-top">
           <p class="text-center"><strong>
-            Selling out for {{ rep.cable_contributions }}
+            Took {{ rep.cable_contributions }} from telecoms
           </strong></p>
         </div> <!-- .fill -->
         <div class="sml-pad-1 sml fill-white">
